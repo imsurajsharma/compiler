e13ef1b98758140355a710001a10eb2b09bb6051
suraj SHARMA <earticleblog007@gmail.com>
2019-08-04 23:36:43 +0530

    Add files via upload

---
+++ compiler/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render,redirect
+import subprocess
+from shell import shell
+
+
+def index(request):
+
+    if request.method =='POST':
+        datas= request.POST['compile']
+        user= request.POST['input']
+        input_list= list()
+
+
+        with open('input.txt','w') as file:
+            file.write(user)
+        with open('input.txt','r') as file:
+
+            for line in file.readlines():
+                input_list.append(line)
+        print(input_list)
+
+        with open('script.py','w') as file:
+            file.write(datas)
+        with open('script.py','r+') as file:
+            data = file.read()
+            ot = shell("python script.py "+'> out.txt',has_input=True)
+            ot.write(input_list[1])
+
+            ott = ot.output()
+
+        with open('out.txt','r') as file:
+            o=file.read()
+        return render(request,'index.html',{'dic':ott})
+    else:
+        return render(request,'index.html')
